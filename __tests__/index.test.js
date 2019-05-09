c1c011c3067d8abb9d151a8aa6769e34231b4533
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2019-05-09 23:13:51 +0300

    add support yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,28 +1,36 @@
-import * as GenDiff from '../src';
+import { compare } from '../src';
+import { readFile, parse } from '../src/parsers';
 
-test('read file to string', () => {
-  const json = [
-    '{\n',
-    '  "host": "hexlet.io",\n',
-    '  "timeout": 50,\n',
-    '  "proxy": "123.234.53.22",\n',
-    '  "follow": false\n',
-    '}\n',
-  ].join('');
+const pathToData = '__tests__/__fixtures__/';
 
-  expect(GenDiff.readFile('__fixtures__')).toBe(null);
-  expect(GenDiff.readFile('__tests__/__fixtures__/before.json')).toBe(json);
+test('read file', () => {
+  const json = {
+    content: [
+      '{\n',
+      '  "host": "hexlet.io",\n',
+      '  "timeout": 50,\n',
+      '  "proxy": "123.234.53.22",\n',
+      '  "follow": false\n',
+      '}\n',
+    ].join(''),
+    format: 'json',
+  };
+
+  expect(() => readFile('__tests__')).toThrow('illegal operation on a directory');
+  expect(() => readFile('wrong path')).toThrow('no such file or directory');
+  expect(readFile(`${pathToData}before.json`)).toEqual(json);
 });
 
-test('convert JSON string to Object', () => {
+test('parse data to obj', () => {
   const obj = {
     host: 'hexlet.io',
     timeout: 50,
     proxy: '123.234.53.22',
     follow: false,
   };
-  expect(GenDiff.jsonToObj(GenDiff.readFile('__tests__/__fixtures__/before.json'))).toEqual(obj);
-  expect(GenDiff.jsonToObj('Wrong format')).toEqual(null);
+  expect(parse(readFile(`${pathToData}before.json`))).toEqual(obj);
+  expect(() => parse('wrong format')).toThrow('is not a function');
+  expect(() => parse(readFile(`${pathToData}before-error.json`))).toThrow('Unexpected token');
 });
 
 test('compare two objects', () => {
@@ -35,7 +43,20 @@ test('compare two objects', () => {
     '+ verbose: true',
   ].join('\n');
 
-  const obj1 = GenDiff.jsonToObj(GenDiff.readFile('__tests__/__fixtures__/before.json'));
-  const obj2 = GenDiff.jsonToObj(GenDiff.readFile('__tests__/__fixtures__/after.json'));
-  expect(GenDiff.compare(obj1, obj2)).toBe(expected);
+  const obj1 = parse(readFile(`${pathToData}before.json`));
+  const obj2 = parse(readFile(`${pathToData}after.yml`));
+  expect(compare(obj1, obj2)).toBe(expected);
 });
+
+// test('default', () => {
+//   genDiff(`${pathToData}before.json`, `${pathToData}after.yml`);
+//   console.log = jest.fn();
+//   genDiff(`${pathToData}before.json`, `${pathToData}after.yml`);
+//   expect(console.log).toBe('xzxz');
+// });
+
+// test('Tests my console.log', () => {
+//   console.log = jest.fn();
+//   console.log('My test is working with console.log');
+//   expect(console.log).toHaveBeenCalledWith('My test is working with console.log1');
+// });
