d49c2452796aa450fffc8be75685bae3e7eaae68
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2019-05-11 02:13:33 +0300

    add depth comparison

--- src/index.js
+++ src/index.js
@@ -2,36 +2,48 @@ import _ from 'lodash';
 import { readFile, parse } from './parsers';
 
 const compare = (obj1, obj2) => {
-  const result1 = Object.entries(obj1)
+  const ast1 = Object.entries(obj1)
     .reduce((acc, [key, value]) => {
       if (!_.has(obj2, key)) {
-        return [...acc, { key, value, status: '-' }];
+        return { ...acc, [`- ${key}`]: value };
       }
-      if (value !== obj2[key]) {
-        return [
-          ...acc,
-          { key, value, status: '-' },
-          { key, value: obj2[key], status: '+' },
-        ];
+      if (_.isEqual(value, obj2[key])) {
+        return { ...acc, [`  ${key}`]: value };
       }
-      return [...acc, { key, value, status: ' ' }];
-    }, []);
+      if (_.isPlainObject(value) && _.isPlainObject(obj2[key])) {
+        return { ...acc, [`  ${key}`]: compare(value, obj2[key]) };
+      }
+      return { ...acc, [`- ${key}`]: value, [`+ ${key}`]: obj2[key] };
+    }, {});
 
-  const result2 = Object.keys(obj2)
+  const ast2 = Object.keys(obj2)
     .filter(key => !_.has(obj1, key))
-    .map(key => ({ key, value: obj2[key], status: '+' }));
+    .reduce((acc, key) => ({ ...acc, [`+ ${key}`]: obj2[key] }), {});
 
-  return [...result1, ...result2]
-    .map(({ key, value, status }) => `${status} ${key}: ${value}`)
-    .join('\n');
+  return { ...ast1, ...ast2 };
+};
+
+const render = (ast, depth = 0) => {
+  const str = Object.entries(ast)
+    .reduce((acc, [key, value]) => {
+      // let valueIn;
+      // if (Array.isArray(value)) {
+      //   valueIn = value.map(el =>)
+      // }
+      const valueIn = _.isPlainObject(value) ? render(value, depth + 4) : value;
+      return `${acc}${_.repeat(' ', depth + 2)}${key}: ${valueIn}\n`;
+    }, '');
+
+  return `{\n${str}${_.repeat(' ', depth)}}`;
 };
 
 export default (firstPathToFile, secondPathToFile) => {
   try {
     const firstObj = parse(readFile(firstPathToFile));
     const secondObj = parse(readFile(secondPathToFile));
-    const result = compare(firstObj, secondObj);
-    console.log(result);
+    const ast = compare(firstObj, secondObj);
+    console.log(ast);
+    console.log(render(ast));
   } catch (e) {
     console.error(`${e.name}: ${e.message}`);
   }
