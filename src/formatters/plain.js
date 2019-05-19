374c8051d691f1be15fdb134fbc011c5aaae1632
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2019-05-19 22:19:29 +0300

    testing cc

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,25 +1,44 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
 const showComplexValue = value => (
   value instanceof Object ? '[complex value]' : value
 );
 
 const plain = (diff, parent = []) => diff
-  .map((obj) => {
-    if (_.has(obj, 'children')) {
-      return plain(obj.children, [...parent, obj.key]);
+  .map((node) => {
+    if (node.type === 'changed') {
+      return plain(node.children, [...parent, node.key]);
     }
-    const str = `Property '${[...parent, obj.key].join('.')}'`;
-    if (obj.status === 'removed') {
+    const str = `Property '${[...parent, node.key].join('.')}'`;
+    if (node.type === 'removed') {
       return `${str} was removed`;
     }
-    if (obj.status === 'added') {
-      return `${str} was added with value: '${showComplexValue(obj.value)}'`;
+    if (node.type === 'added') {
+      return `${str} was added with value: '${showComplexValue(node.newValue)}'`;
     }
-    if (obj.status === 'updated') {
-      return `${str} was updated. From '${showComplexValue(obj.value)}' to '${showComplexValue(obj.updateValue)}'`;
+    if (node.type === 'updated') {
+      return `${str} was updated. From '${showComplexValue(node.oldValue)}' to '${showComplexValue(node.newValue)}'`;
     }
-    return null;
-  }).filter(el => el !== null).join('\n');
+    return '';
+  }).filter(el => el !== '').join('\n');
+
+// const plain = (diff, parent = []) => diff
+//   .map((obj) => {
+//     if (_.has(obj, 'children')) {
+//       return plain(obj.children, [...parent, obj.key]);
+//     }
+//     const str = `Property '${[...parent, obj.key].join('.')}'`;
+//     if (obj.status === 'removed') {
+//       return `${str} was removed`;
+//     }
+//     if (obj.status === 'added') {
+//       return `${str} was added with value: '${showComplexValue(obj.value)}'`;
+//     }
+//     if (obj.status === 'updated') {
+//       return `${str} was updated. From '${showComplexValue(obj.value)}' to
+// '${showComplexValue(obj.updateValue)}'`;
+//     }
+//     return '';
+//   }).filter(el => el !== '').join('\n');
 
 export default plain;
--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
 
-const getStatus = (status) => {
-  switch (status) {
-    case 'added':
-      return '+';
-    case 'removed':
-      return '-';
-    case 'updated':
-      return '-';
-    default:
-      return ' ';
-  }
-};
+// const getStatus = (status) => {
+//   switch (status) {
+//     case 'added':
+//       return '+';
+//     case 'removed':
+//       return '-';
+//     case 'updated':
+//       return '-';
+//     default:
+//       return ' ';
+//   }
+// };
 
 const stringify = (value, indent, inline = false) => {
   const arrayToString = arr => arr.map(el => stringify(el, indent, true)).join(', ');
@@ -36,18 +36,42 @@ const stringify = (value, indent, inline = false) => {
 };
 
 const tree = (diff, indent = 0) => {
-  const el = diff.reduce((acc, obj) => {
-    const value = _.has(obj, 'children')
-      ? tree(obj.children, indent + 4)
-      : stringify(obj.value, indent + 4);
-    const property = `${' '.repeat(indent + 2)}${getStatus(obj.status)} ${obj.key}: ${value}`;
-    const updatedProperty = _.has(obj, 'updateValue')
-      ? `${' '.repeat(indent + 2)}+ ${obj.key}: ${stringify(obj.updateValue, indent + 4)}\n`
-      : '';
-    return `${acc}${property}\n${updatedProperty}`;
+  const list = diff.reduce((acc, node) => {
+    if (node.type === 'added') {
+      return `${acc}${' '.repeat(indent + 2)}+ ${node.key}: ${stringify(node.newValue, indent + 4)}\n`;
+    }
+    if (node.type === 'removed') {
+      return `${acc}${' '.repeat(indent + 2)}- ${node.key}: ${stringify(node.oldValue, indent + 4)}\n`;
+    }
+    if (node.type === 'changed') {
+      return `${acc}${' '.repeat(indent + 2)}  ${node.key}: ${tree(node.children, indent + 4)}\n`;
+    }
+    if (node.type === 'unchanged') {
+      return `${acc}${' '.repeat(indent + 2)}  ${node.key}: ${stringify(node.oldValue, indent + 4)}\n`;
+    }
+    return [
+      `${acc}`,
+      `${' '.repeat(indent + 2)}- ${node.key}: ${stringify(node.oldValue, indent + 4)}\n`,
+      `${' '.repeat(indent + 2)}+ ${node.key}: ${stringify(node.newValue, indent + 4)}\n`,
+    ].join('');
   }, '');
 
-  return `{\n${el}${' '.repeat(indent)}}`;
+  return `{\n${list}${' '.repeat(indent)}}`;
 };
+
+// const tree = (diff, indent = 0) => {
+//   const el = diff.reduce((acc, obj) => {
+//     const value = _.has(obj, 'children')
+//       ? tree(obj.children, indent + 4)
+//       : stringify(obj.value, indent + 4);
+//     const property = `${' '.repeat(indent + 2)}${getStatus(obj.status)} ${obj.key}: ${value}`;
+//     const updatedProperty = _.has(obj, 'updateValue')
+//       ? `${' '.repeat(indent + 2)}+ ${obj.key}: ${stringify(obj.updateValue, indent + 4)}\n`
+//       : '';
+//     return `${acc}${property}\n${updatedProperty}`;
+//   }, '');
+
+//   return `{\n${el}${' '.repeat(indent)}}`;
+// };
 
 export default tree;
