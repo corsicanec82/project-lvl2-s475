817bb56745ebabf6d146c9052e68628d5e850bfe
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2019-05-19 16:55:32 +0300

    fix cognitive comlexity

--- src/index.js
+++ src/index.js
@@ -2,50 +2,20 @@ import _ from 'lodash';
 import { readFile, parse, addParser } from './parsers';
 import { addFormatter, getFormatter } from './formatters';
 
-// const genDiff = (data1, data2) => {
-//   const part1 = Object.entries(data1)
-//     .map(([key, value]) => {
-//       const element = {
-//         key, value, children: false, status: 'unchanged',
-//       };
-//       if (!_.has(data2, key)) {
-//         return { ...element, status: 'removed' };
-//       }
-//       if (_.isEqual(value, data2[key])) {
-//         return element;
-//       }
-//       if (_.isPlainObject(value) && _.isPlainObject(data2[key])) {
-//         return { ...element, value: genDiff(value, data2[key]), children: true };
-//       }
-//       return { ...element, status: 'updated', updateValue: data2[key] };
-//     });
-
-//   const part2 = Object.keys(data2)
-//     .filter(key => !_.has(data1, key))
-//     .map(key => (
-//       {
-//         key, value: data2[key], status: 'added', children: false,
-//       }
-//     ));
-
-//   return [...part1, ...part2];
-// };
-
 const genDiff = (data1, data2) => {
   const part1 = Object.entries(data1)
     .map(([key, value]) => {
-      const element = { status: 'unchanged' };
+      const element = { key, value, status: 'unchanged' };
       if (!_.has(data2, key)) {
-        element.status = 'removed';
-      } else if (!_.isEqual(value, data2[key])) {
-        if (_.isPlainObject(value) && _.isPlainObject(data2[key])) {
-          element.children = genDiff(value, data2[key]);
-        } else {
-          element.status = 'updated';
-          element.updateValue = data2[key];
-        }
+        return { ...element, status: 'removed' };
       }
-      return { key, value, ...element };
+      if (_.isEqual(value, data2[key])) {
+        return element;
+      }
+      if (_.isPlainObject(value) && _.isPlainObject(data2[key])) {
+        return { ...element, children: genDiff(value, data2[key]) };
+      }
+      return { ...element, status: 'updated', updateValue: data2[key] };
     });
 
   const part2 = Object.keys(data2)
@@ -54,6 +24,30 @@ const genDiff = (data1, data2) => {
 
   return [...part1, ...part2];
 };
+
+// const genDiff = (data1, data2) => {
+//   const part1 = Object.entries(data1)
+//     .map(([key, value]) => {
+//       const element = { status: 'unchanged' };
+//       if (!_.has(data2, key)) {
+//         element.status = 'removed';
+//       } else if (!_.isEqual(value, data2[key])) {
+//         if (_.isPlainObject(value) && _.isPlainObject(data2[key])) {
+//           element.children = genDiff(value, data2[key]);
+//         } else {
+//           element.status = 'updated';
+//           element.updateValue = data2[key];
+//         }
+//       }
+//       return { key, value, ...element };
+//     });
+
+//   const part2 = Object.keys(data2)
+//     .filter(key => !_.has(data1, key))
+//     .map(key => ({ key, value: data2[key], status: 'added' }));
+
+//   return [...part1, ...part2];
+// };
 
 const render = (diff, format) => getFormatter(format)(diff);
 
